806591c98c9895e07d4368fe009ab491db8a3706
Yung Lai <stellvus@gmail.com>
2022-01-22 22:28:59 -0600

    Setup the API calls for image

--- api/controllers/image.py
+++ api/controllers/image.py
@@ -1,5 +1,8 @@
+import flask_rebar
+
 from api.app import registry
-from api.schemas.image import ImageSchema
+from api.schemas.image import ImageSchema, ImageCreationSchema
+from uuid import uuid4, UUID
 
 BASE_URL = "/images"
 
@@ -10,4 +13,31 @@ BASE_URL = "/images"
     response_body_schema={200: ImageSchema()},
 )
 def get_images():
-    return {"id": "image_id"}
+    return {"id": uuid4()}
+
+
+@registry.handles(
+    method="GET",
+    rule=f"{BASE_URL}/<uuid:image_id>",
+    response_body_schema={200: ImageSchema()},
+)
+def get_image(image_id: UUID):
+    return {"id": image_id}
+
+
+@registry.handles(
+    method="POST",
+    rule=BASE_URL,
+    request_body_schema=ImageCreationSchema,
+    response_body_schema={200: ImageSchema()},
+)
+def save_image():
+    body = flask_rebar.get_validated_body()
+    image = {"id": uuid4()}
+
+    if 'label' in body:
+        image['label'] = body['label']
+    else:
+        image['label'] = "generated_label"
+
+    return image
--- api/schemas/image.py
+++ api/schemas/image.py
@@ -1,6 +1,24 @@
-from flask_rebar import ResponseSchema
-from marshmallow import fields
+from typing import Any
+
+from flask_rebar import ResponseSchema, RequestSchema
+from marshmallow import fields, validates_schema, ValidationError
 
 
 class ImageSchema(ResponseSchema):
-    id = fields.String()
+    id = fields.UUID()
+    label = fields.String()
+
+
+class ImageCreationSchema(RequestSchema):
+    file = fields.String()
+    url = fields.URL()
+    label = fields.String(allow_none=True)
+    enable_object_detection = fields.Boolean(default=False)
+
+    @validates_schema
+    def validate_file_or_url_provided(self, data: Any, **kwargs) -> None:
+        if 'file' in data and 'url' in data:
+            raise ValidationError('Only file or URL is needed.')
+
+        if 'file' not in data and 'url' not in data:
+            raise ValidationError('Must provide file or URL for image.')
